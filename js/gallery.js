ea43c718b6706af200746ec4da2f52fe24638537
Player <Alice@Wonderland.local>
2019-09-22 14:24:34 +0330

    Finish the JavaScript gallery.

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,35 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+    thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+
+    // Set clicked image as main image.
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let newAltSrc = thumbnail.alt;
+
+    mainImage.setAttribute("src", newImageSrc);
+    mainImage.setAttribute("alt", newAltSrc);
+
+    // Change which image is current.
+    let currentClass = document.querySelector(".current");
+    let parentClass = thumbnail.parentNode;
+
+    currentClass.classList.remove("current");
+    parentClass.classList.add("current");
+
+    // Update image info.
+    let galleryInfo = document.querySelector("#gallery-info");
+    let title       = galleryInfo.querySelector(".title");
+    let description = galleryInfo.querySelector(".description");
+
+    title.innerHTML       = thumbnail.dataset.title;
+    description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
